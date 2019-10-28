ac0240a7b5c3d21129a61e640bb6d30463bbd8f7
Sheharyar Naseer <hello@sheharyar.me>
2019-10-28 04:14:01 -0500

    Fix compiler warnings for walk function clause
    Fixes compiler warnings for unused variables in the `walk` function clause

--- lib/replacing_walk.ex
+++ lib/replacing_walk.ex
@@ -67,7 +67,7 @@ defmodule ReplacingWalk do
   def walk(%{}, _, _), do: %{}
 
   # kv tuples (very common in config)
-  def walk(t = {k,v}, recognize, transform) do
+  def walk({_k, _v} = t, recognize, transform) do
     t = maybe_transform_leaf(t, recognize, transform)
     if is_tuple t do
       {k, v} = t
